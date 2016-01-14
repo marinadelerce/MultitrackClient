8ff774eed866835bef19fc959e186e569b3620a4
Marina Delerce-Mauris <marina.delerce@gmail.com>
2016-01-14 08:30:07 +0100

    archi client avec yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name multitrackClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the multitrackClientApp
+ */
+angular.module('multitrackClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name multitrackClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the multitrackClientApp
+ */
+angular.module('multitrackClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
