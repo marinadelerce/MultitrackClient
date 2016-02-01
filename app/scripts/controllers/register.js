22fb690014e205b0563f2215db84216e233aac52
SoniaTual <sonia.tual@orange.fr>
2016-02-01 18:21:30 +0100

    création utilisateur

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,3 +1,26 @@
 /**
  * Created by marina on 28/01/16.
  */
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name multitrackClientApp.controller:MixCtrl
+ * @description
+ * # MixCtrl
+ * Controller of the multitrackClientApp
+ */
+angular.module('multitrackClientApp')
+  .controller('RegisterCtrl', function ($scope, $http, Constants, $location) {
+    $scope.createUser = function(){
+      console.log($scope.pseudo);
+      console.log($scope.pwd);
+      console.log($scope.right);
+      $http.post(Constants.backendUrl + 'user', {"name":$scope.pseudo,"pwd":$scope.pwd, "right":$scope.right })
+        .then(function(res){
+          //stocker tokken
+        }, function(error){
+          console.log(error);
+        });
+    };
+  });
