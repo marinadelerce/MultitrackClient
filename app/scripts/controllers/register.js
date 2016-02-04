2a1200f8210bfb0fdccfadd0395438bce6c3c550
SoniaTual <sonia.tual@orange.fr>
2016-02-04 09:04:18 +0100

    stockage info user + token suite à la création d'un utilisateur

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -11,16 +11,43 @@
  * Controller of the multitrackClientApp
  */
 angular.module('multitrackClientApp')
-  .controller('RegisterCtrl', function ($scope, $http, Constants, $location) {
+  .controller('RegisterCtrl', function ($rootScope, $scope, $http, Constants, $location, $cookies) {
     $scope.createUser = function(){
       console.log($scope.pseudo);
       console.log($scope.pwd);
       console.log($scope.right);
       $http.post(Constants.backendUrl + 'user', {"name":$scope.pseudo,"pwd":$scope.pwd, "right":$scope.right })
         .then(function(res){
-          //stocker tokken
+          //stocker token
+          $cookies.remove('token');
+          $cookies.remove('userName');
+          $http.post(Constants.backendUrl + Constants.userPath + '/' +  Constants.connexionPath, {'name' : $scope.pseudo,"pwd":$scope.pwd})
+            .then(function(res) {
+              console.log(res.data);
+              $rootScope.user ={};
+              $rootScope.user.name = $scope.pseudo;
+              $rootScope.user.right = $scope.right;
+
+              console.log($rootScope.user);
+
+              console.log($cookies);
+              $cookies.put('token', res.data);
+              $cookies.put('userName', $scope.pseudo);
+              var name = $cookies.get('userName');
+              console.log(name);
+              var token = $cookies.get('token');
+              console.log(token);
+
+              $location.path('/mix');
+            },function(error) {
+              //TODO: Manage error during post => display error message
+              console.log("fail login during POST");
+              alert("Login ou mot de passe incorrect !");
+              console.log(error);
+            });
         }, function(error){
           console.log(error);
+          alert("impossible de créer l'utilisateur");
         });
     };
   });
