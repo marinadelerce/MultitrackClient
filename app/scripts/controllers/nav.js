d1e6e5ef873dcadf0cf514d46ab6534aca0369b7
Marina Delerce-Mauris <marina.delerce@gmail.com>
2016-02-04 09:13:05 +0100

    deconnexion

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -11,8 +11,15 @@
  * Controller of the multitrackClientApp
  */
 angular.module('multitrackClientApp')
-  .controller('NavCtrl', ['$scope','$location', function ($scope, $location) {
+  .controller('NavCtrl', ['$rootScope','$scope','$location', '$cookies', function ($rootScope,$scope, $location, $cookies) {
     $scope.isActive = function(destination){
       return destination === $location.path();
+    };
+
+    $scope.logout = function() {
+      $cookies.remove('token');
+      $cookies.remove('userName');
+      $rootScope.user = null;
+      $location.path('/');
     };
   }]);
